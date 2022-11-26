db5b00c779e384eea7884f796d8afd009905f30d
Luc Borris <lucborris@gmail.com>
2022-11-26 12:08:04 -0700

    change to jsx

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <>
+      <h1 id="free_press">Free Press!</h1>
+      <form class="form">
+        <input type="text" id="website_to_be_crawled" />
+        <input type="submit" />
+      </form>
+    </>
+  );
+}
