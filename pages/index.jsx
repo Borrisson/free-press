a738fd30f74bb52c043962a14b2d7c3642933c74
Sava-B <savab2164@gmail.com>
2022-11-26 13:32:12 -0700

    edit

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,8 +5,8 @@ export default function Home() {
   const [value, setValue] = useState("");
   return (
     <div className={styles.container}>
-      <h1 id="free_press">Free Press!</h1>
-      <form className="form" className={styles.form}>
+      <h1 className="free_press">Free Press!</h1>
+      <form className={styles.form}>
         <label for="fname">Website You Want To Access: </label>
         <input
           type="text"
