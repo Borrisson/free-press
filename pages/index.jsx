08e501b6663cbc7b8353a7eb517dcc660b5cf42b
Luc Borris <lucborris@gmail.com>
2022-11-26 15:17:52 -0700

    innerHTML route

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,20 @@
 import styles from "../styles/Home.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function Home() {
   const [url, setUrl] = useState("");
+  const [html, setHtml] = useState("");
+
+  useEffect(() => {
+    if (html) {
+      const domParser = new DOMParser();
+      const htmlDocument = domParser.parseFromString(html, "text/html");
+      const article = htmlDocument.querySelector("article");
+      document.getElementById("html").innerHTML = `${article}`;
+    }
+  }, [html]);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.freePress}>Free Press!</h1>
@@ -20,12 +31,13 @@ export default function Home() {
           onClick={async (e) => {
             e.preventDefault();
             const { data } = await axios.post("/api/article", { url });
-            console.log(data);
+            setHtml(data);
           }}
         >
           Submit
         </button>
       </form>
+      {html && <div id="html" />}
     </div>
   );
 }
